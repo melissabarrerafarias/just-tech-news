1c97f0ca0314f6109c6e2031d6130e0eb6069082
unknown <melissikiss@gmail.com>
2020-12-14 20:11:34 -0800

    added CRUD routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,21 @@
 const User = require('./User');
 const Post = require("./Post");
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
+
+
+// Post.belongsTo(User, {
+//     foreignKey: 'user_id',
+// });
+// In this statement, we are defining the relationship of the Post model to the User. The constraint we impose here is that a post can
+// belong to one user, but not many users. Again, we declare the link to the foreign key, which is designated at user_id in the Post 
+// model.
